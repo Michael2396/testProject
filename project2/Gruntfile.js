3f0ad163947c669c761637f0fa83ae4cc6f7e03b
Mykhailo <mykhailo.onufriiev@md.p2h.com>
2022-12-28 00:49:39 +0200

    upd

---
+++ project2/Gruntfile.js
@@ -0,0 +1,136 @@
+module.exports = function(grunt) {
+
+  const sass = require('node-sass');
+
+  require('load-grunt-tasks')(grunt);
+
+var PathConfig = require('./grunt-settings.js');
+
+  // tasks
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    config: PathConfig,
+
+    //clean files
+    clean: {
+      options: { force: true },
+      temp: {
+        src: ["<%= config.cssDir %>**/*.map", "<%= config.cssMainFileDir %><%= config.cssMainFileName %>.css.map"]
+      }
+    },
+
+    //sass
+    sass: {
+      options: PathConfig.hasBower,
+      dev: {
+        options: {
+          implementation: sass,
+          sourceMap: true,
+          style: 'nested'
+        },
+        files: [
+          {
+            expand: true,
+            cwd: '<%= config.sassDir %>',
+            src: ['**/*.scss', '!<%= config.sassMainFileName %>.scss'],
+            dest: '<%= config.cssDir %>',
+            ext: '.css'
+          },
+          {src: '<%= config.sassDir %><%= config.sassMainFileName %>.scss', dest: '<%= config.cssMainFileDir %><%= config.cssMainFileName %>.css'}
+        ]
+      },
+      dist: {
+        options: {
+          implementation: sass,
+          sourceMap: false,
+          style: 'nested'
+        },
+        files: [
+          {
+            expand: true,
+            cwd: '<%= config.sassDir %>',
+            src: ['**/*.scss', '!<%= config.sassMainFileName %>.scss'],
+            dest: '<%= config.cssDir %>',
+            ext: '.css'
+          },
+          {src: '<%= config.sassDir %><%= config.sassMainFileName %>.scss', dest: '<%= config.cssMainFileDir %><%= config.cssMainFileName %>.css'}
+        ]
+      },
+      min: {
+        options: {
+          implementation: sass,
+          sourceMap: false,
+          outputStyle: 'compressed'
+        },
+        files: [
+          {
+            expand: true,
+            cwd: '<%= config.sassDir %>',
+            src: ['**/*.scss', '!<%= config.sassMainFileName %>.scss'],
+            dest: '<%= config.cssDir %>',
+            ext: '.min.css'
+          },
+          {src: '<%= config.sassDir %><%= config.sassMainFileName %>.scss', dest: '<%= config.cssMainFileDir %><%= config.cssMainFileName %>.min.css'}
+        ]
+      }
+    },
+
+    //watcher project
+    watch: {
+      options: {
+        debounceDelay: 1,
+        // livereload: true,
+      },
+      css: {
+        files: ['<%= config.sassDir %>**/*.scss'],
+        tasks: ['sass:dev'],
+        options: {
+            spawn: false,
+        }
+      }
+    },
+
+    //Keep multiple browsers & devices in sync when building websites.
+    browserSync: {
+      dev: {
+        bsFiles: {
+          src : ['*.html', '*.php', '<%= config.cssDir %>*.css', '*.css']
+        },
+        options: {
+          // server: {
+          //   baseDir: "../",
+          //   index: "index.html",
+          //   directory: true
+          // },
+          proxy: '<%= config.browserSyncUrl %>',
+          open: 'external',
+          host: '<%= config.browserSyncUrl %>',
+          watchTask: true
+        }
+      }
+    },
+
+
+  });
+
+// run task
+//dev 
+  //watch
+  grunt.registerTask('w', ['watch']);
+  //browser sync
+  grunt.registerTask('bs', ['browserSync']);
+
+  //watch + browser sync
+  grunt.registerTask('dev', ['browserSync', 'watch']);
+  
+  grunt.registerTask('default', ['dev']);
+
+//finally 
+  //css beautiful
+  grunt.registerTask('cssbeauty', ['sass:dist']);
+
+  //final build
+  grunt.registerTask('dist', ['clean:temp', 'cssbeauty']);
+
+};
---
+++ project2/grunt-settings.js
@@ -0,0 +1,9 @@
+module.exports = {
+  sassDir:          'scss/',
+  sassMainFileName: 'style',
+  cssDir:           'css/',
+  cssMainFileDir:   './',
+  cssMainFileName:  'style',
+  jsDir:            'js/',
+  browserSyncUrl:   'p.test',
+};
